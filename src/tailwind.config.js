8578099ba94b0c20a987be762ef5a3a0524dfa82
mu-tech8 <kawamuratakuya@mu.local>
2021-10-03 15:14:02 +0900

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                orange: "#FF710B",
+                sepia: "#FCF8EB",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
